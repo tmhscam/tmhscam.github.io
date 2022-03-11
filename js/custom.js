6c8702171ba90affc4822c45c0c70f8aa53cf754
tmhscam <61848969+tmhscam@users.noreply.github.com>
2022-03-11 17:55:47 -0600

    upload 1

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+
+$(function () {
+
+	"use strict";
+
+	/* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	setTimeout(function () {
+		$('.loader_bg').fadeToggle();
+	}, 1500);
+
+	$(document).ready(function () {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+
+
+
+});
